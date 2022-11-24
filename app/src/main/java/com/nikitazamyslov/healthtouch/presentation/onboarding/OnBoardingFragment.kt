0ab373b935fa789c5a9e262adc9e70fb075f45e5
Nikita Zamyslov <nikitazamyslov@mail.ru>
2022-11-24 21:12:47 +0300

    Squashed commit of the following:
    commit 02d8e6c930fb788d33a5ec31e6f7a5de7e4fa5b8
    Author: Nikita Zamyslov <nikitazamyslov@mail.ru>
    Date:   Thu Nov 24 21:07:51 2022 +0300
    
        Added Splash Screen

---
+++ app/src/main/java/com/nikitazamyslov/healthtouch/presentation/onboarding/OnBoardingFragment.kt
@@ -0,0 +1,33 @@
+package com.nikitazamyslov.healthtouch.presentation.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nikitazamyslov.healthtouch.R
+import com.nikitazamyslov.healthtouch.databinding.FragmentOnBoardingBinding
+import com.nikitazamyslov.healthtouch.databinding.FragmentSplashScreenBinding
+
+class OnBoardingFragment : Fragment() {
+
+    private var _binding: FragmentOnBoardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOnBoardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
