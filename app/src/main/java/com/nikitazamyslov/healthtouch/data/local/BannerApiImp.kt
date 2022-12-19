a27661878b45659296a03f1c7f2749b96f1dfded
Nikita Zamyslov <nikitazamyslov@mail.ru>
2022-12-19 07:28:08 +0300

    Добавлены баннеры

--- app/src/main/java/com/nikitazamyslov/healthtouch/data/local/BannerApiImp.kt
+++ app/src/main/java/com/nikitazamyslov/healthtouch/data/local/BannerApiImp.kt
@@ -12,9 +12,24 @@ class BannerApiImp @Inject constructor() : IBannerApi {
             "https://lepsija.cz/wp-content/uploads/2021/04/workout-composition-with-healthy-food.jpg"
         ),
         BannerDao(
-            "2", "Result of all measurements",
-            "You can see graphics and recommends",
+            "2", "General recommendations",
+            "Get regular check-ups with a doctor.",
             "https://kvd-moskva.ru/sites/default/files/1-144.jpg"
+        ),
+        BannerDao(
+            "3", "General recommendations",
+            "Try to be less nervous and try meditation.",
+            "https://cleandan.ru/images/content2/st8-1.jpg"
+        ),
+        BannerDao(
+            "4", "General recommendations",
+            "Don't forget about rest.",
+            "https://mobimg.b-cdn.net/v3/fetch/dc/dc679df7fe3584c5b3dd8c0f2cf2506e.jpeg"
+        ),
+        BannerDao(
+            "5", "General recommendations",
+            "Watch your health.",
+            "https://digitalhealthbuzz.com/wp-content/uploads/2022/02/Depositphotos_26751869_L.jpg"
         )
     )
 
